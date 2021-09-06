e892b025713ecd6a8729d39bd2745cae626243d4
lucas blommers <lucas-blommers@live.nl>
2021-09-06 11:18:56 +0200

    Invoer van NAW toegevoegd in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hallo, wat is uw volledige naam?")
+name=input()
+
+print("En uw adres?")
+address = input()
+
+print("En uw postcode?")
+zipcode = input()
+
+print("En tot slot uw woonplaats")
+hometown = input()
+
+print("------------------------------------------------")
+print("|    Naam        : " + name)
+print("|    Adres       : " + address)
+print("|    Postcode    : " + zipcode)
+print("|    Woonplaats  : " + hometown)
+print("------------------------------------------------")
